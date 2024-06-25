de2f40f7cb24b5bfa9e95dcb78233ab3be24a1f0
Amos Oluoch <aoluoch271@gmail.com>
2024-06-25 23:58:44 +0300

    art

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
 // Write your code here
+let num1 = 62,
+num2 = 1;
+let multiply = num1 * num2;
+console.log(multiply)
+
+let random = 0+1;
+console.log(random)
+
+let num3 = 9;
+let num4 = 5;
+let mod = num3 % num4;
+console.log(mod);
+
+let max = Math.max(1, 7, 14, 20, 19);
--- test/indexTest.js
+++ test/indexTest.js
@@ -3,7 +3,7 @@ require ( './helpers.js' );
 const chai = require('chai')
 const expect = chai.expect;
 
-describe('index.js', function() {
+describe('index.js', function() { 
   describe('multiply', function() {
     it("is an equation whose multiplied results will equal 62", function() {
       expect(num1).to.be.a('number')
